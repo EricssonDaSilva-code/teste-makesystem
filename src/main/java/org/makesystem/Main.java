878aee6ab1f1933d984240d962b23c126ff9cfc7
EricssonDasilva-code <dasilva.ericsson@gmail.com>
2022-10-04 22:48:39 -0300

    Crianda uma função unica para tratar o array e colocada na classe Person

--- src/main/java/org/makesystem/Main.java
+++ src/main/java/org/makesystem/Main.java
@@ -2,32 +2,27 @@ package org.makesystem;
 
 import org.makesystem.entities.Person;
 
-import javax.swing.text.DateFormatter;
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.format.ResolverStyle;
-import java.util.*;
+import java.util.Date;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
         Set<Person> personSet = new TreeSet<>();
+        Person person = new Person();
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        String path = "C:\\Users\\dasil\\IdeaProjects\\teste-makesystem\\src\\main\\java\\org\\makesystem\\desafio_junior_txt.txt";
+        String path = "C:\\Users\\dasil\\IdeaProjects\\teste-makesystem\\src\\main\\java\\org\\makesystem\\desafio_junior.csv";
         String outPath = "C:\\Users\\dasil\\IdeaProjects\\teste-makesystem\\src\\main\\java\\org\\makesystem\\desafio_junior3.txt";
-        boolean r1, r2, r3, r4 ;
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-8"))) {
+        boolean teste;
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(path), "ISO-8859-1"))) {
             String line = bufferedReader.readLine();
             while (line != null) {
                 String[] array = line.split(";");
-                r1 = testName(array[0]);
-                r2 = testDocument(array[1]);
-                r3 = testBirthDate(array[2]);
-                r4 = testPhoneNumber(array[3]);
-                if (!r1 || !r2 || !r3 || !r4) {
+                teste = person.testArray(array);
+                if (!teste) {
                     line = bufferedReader.readLine();
                 }
                 else {
@@ -63,57 +58,6 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-    }
-    public static Boolean testName(String name) {
-        String[] nameTeste = name.trim().split(" ");
-        boolean resposta = true;
-        if (nameTeste.length < 2) {
-            resposta = false;
-        }
-        else {
-            for (int i = 0; i < nameTeste.length; i ++) {
-                if (nameTeste[i].replaceAll("[^a-zA-Z]*", "").length() <= 1) {
-                    resposta = false;
-                }
-            }
-        }
-
-
-        return resposta;
-    }
-
-    public static boolean testDocument(String document) {
-        String documentTest = document.trim().replaceAll("[^0-9]*", "");
-        boolean resposta = true;
-        if (documentTest.length() == 11 || documentTest.length() == 14) {
-            resposta = true;
-        }
-        else {
-            resposta = false;
-        }
-        return resposta;
-    }
-
-    public static boolean testBirthDate(String birthDate) {
-        String dateFormat = "dd/MM/yyyy";
-
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter
-                .ofPattern(dateFormat);
-        try {
-            LocalDate date = LocalDate.parse(birthDate, dateTimeFormatter);
-            return true;
-        } catch (DateTimeParseException e) {
-            return false;
-        }
-
-    }
-
-    public static boolean testPhoneNumber(String phoneNumber) {
-        String phoneNumberTest = phoneNumber.trim().replaceAll("[^0-9]*", "");
-        if (phoneNumberTest.length() < 11) {
-            return false;
-        }
-        return true;
     }
 
 }
--- src/main/java/org/makesystem/entities/Person.java
+++ src/main/java/org/makesystem/entities/Person.java
@@ -4,11 +4,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 import java.util.Objects;
 
-public class Person implements Serializable, Comparable<Person> {
-    private static final long serialVersionUID = 1L;
+public class Person implements Comparable<Person> {
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
@@ -58,6 +60,40 @@ public class Person implements Serializable, Comparable<Person> {
 
     public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public static boolean testArray(String[] array) {
+        String[] nameTeste = array[0].trim().split(" ");
+        if (nameTeste.length < 2) {
+            return false;
+        }
+        else {
+            for (int i = 0; i < nameTeste.length; i ++) {
+                if (nameTeste[i].replaceAll("[^a-zA-Z]*", "").length() <= 1) {
+                    return false;
+                }
+            }
+        }
+        String documentTest = array[1].trim().replaceAll("[^0-9]*", "");
+        if (!(documentTest.length() == 11 || documentTest.length() == 14)) {
+            return false;
+        }
+
+        String phoneNumberTest = array[3].trim().replaceAll("[^0-9]*", "");
+        if (phoneNumberTest.length() < 11) {
+            return false;
+        }
+
+        String birthdateTest = array[2];
+        String dateFormat = "dd/MM/yyyy";
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormat);
+        try {
+            LocalDate date = LocalDate.parse(birthdateTest, dateTimeFormatter);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+
     }
 
     @Override
